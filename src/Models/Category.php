8e9092e718d0e3b5912441657f8881b46e1e169e
amory traoré <50603437+AmoryTraore@users.noreply.github.com>
2021-05-04 14:54:21 +0000

    Update Category.php

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Traore\Actualite\Models;
+namespace AmoryTraore\Actualite\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
